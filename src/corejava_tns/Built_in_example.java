556159e08c48a1dc82b7a7c6f53bcfb620678406
Aishwarya shetty <aishwaryashettyshetty3@gmail.com>
2025-08-13 18:00:40 +0530

    added

---
+++ src/corejava_tns/Built_in_example.java
@@ -0,0 +1,16 @@
+package corejava_tns;
+
+public class Built_in_example {
+    public static void main(String[] args) {
+        String greeting="Hello Aishwarya";
+        System.out.println("Original String -"+greeting);
+        System.out.println("Upper case -"+greeting.toUpperCase());
+        System.out.println("substring -"+greeting.substring(6));
+
+        int max=Math.max(10,20);
+        double sqrt=Math.sqrt(30.0);
+        System.out.println("max number "+ max);
+        System.out.println("sqrt "+sqrt);
+    }
+
+}
